80014877c5be36bb7fd268ec491114a44e389e00
Tieshun Roquerre <tieshun.roquerre@gmail.com>
2015-08-31 17:15:58 -0700

    Create StringSlice type

---
+++ stringslice.go
@@ -0,0 +1,90 @@
+package stringslice
+
+import (
+	"bytes"
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	// unquoted array values must not contain: (" , \ { } whitespace NULL)
+	// and must be at least one char
+	unquotedChar  = `[^",\\{}\s(NULL)]`
+	unquotedValue = fmt.Sprintf("(%s)+", unquotedChar)
+
+	// quoted array values are surrounded by double quotes, can be any
+	// character except " or \, which must be backslash escaped:
+	quotedChar  = `[^"\\]|\\"|\\\\`
+	quotedValue = fmt.Sprintf("\"(%s)*\"", quotedChar)
+
+	// an array value may be either quoted or unquoted:
+	arrayValue = fmt.Sprintf("(?P<value>(%s|%s))", unquotedValue, quotedValue)
+
+	// Array values are separated with a comma IF there is more than one value:
+	arrayExp = regexp.MustCompile(fmt.Sprintf("((%s)(,)?)", arrayValue))
+
+	valueIndex int
+
+	InvalidScanSource = errors.New("Scan source was not []bytes")
+)
+
+// Find the index of the 'value' named expression
+func init() {
+	for i, subexp := range arrayExp.SubexpNames() {
+		if subexp == "value" {
+			valueIndex = i
+			break
+		}
+	}
+}
+
+type StringSlice []string
+
+func (s *StringSlice) Scan(src interface{}) error {
+	asBytes, ok := src.([]byte)
+	if !ok {
+		return InvalidScanSource
+	}
+
+	asString := string(asBytes)
+	parsed := parseArray(asString)
+	(*s) = StringSlice(parsed)
+
+	return nil
+}
+
+func (s StringSlice) Value() (driver.Value, error) {
+	if len(s) == 0 {
+		return nil, nil
+	}
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("{")
+	last := len(s) - 1
+	for i, val := range s {
+		buffer.WriteString(strconv.Quote(val))
+		if i != last {
+			buffer.WriteString(",")
+		}
+	}
+	buffer.WriteString("}")
+
+	return string(buffer.Bytes()), nil
+}
+
+func parseArray(array string) []string {
+	var results []string
+	matches := arrayExp.FindAllStringSubmatch(array, -1)
+	for _, match := range matches {
+		s := match[valueIndex]
+		// the string _might_ be wrapped in quotes, so trim them:
+		s = strings.Trim(s, "\"")
+		results = append(results, s)
+	}
+	return results
+}
